be67003652976ae08a38697f92e9e7ca7904a031
Luis Diaz <luisdh.8@outlook.com>
2024-10-23 12:29:08 -0600

    Book
    Creación de Book con onclick y css

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import SplashScreen from './SplashScreen.jsx'
 import GetStarted from './GetStarted.jsx'
 import SignIn from './SignIn.jsx'
 import HomePage from './HomePage.jsx'
+import Book from './Book.jsx'
 import './App.css';
 
 function App() {
@@ -15,6 +16,8 @@ function App() {
 
   const handleGetStarted = () => setStep(2);
   const handleLogin = () => setStep(3);
+  const handleBook = () => setStep(4);
+
 
   return (
     <>
@@ -22,6 +25,7 @@ function App() {
       {step === 1 && <GetStarted onClick={handleGetStarted} />}
       {step === 2 && <SignIn onLogin={handleLogin} />}
       {step === 3 && <HomePage />}
+      {step === 4 && <Book onClickHome={handleHome} onClickDeals={handleDeals} onClickAccount={handleAccount}/>}
     </>
   );
 }
